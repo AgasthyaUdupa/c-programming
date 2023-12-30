44c2d826aa15ddf0c8e785696b6b91b3eef0d1ba
Agasthya Udupa <agasthyauduparajkumar2001@gmail.com>
2023-12-30 13:40:29 +0530

    second

---
+++ third.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n,c,s=0,r;
+	printf("enter the number: ");
+	scanf("%d",&n);
+	c=n;
+	while(n>0)
+	{
+		r=n%10;
+		s=r+(s*10);
+		n=n/10;
+	}
+	if(c==s)
+	printf("palindrome number");
+	else
+	printf("not a palindrome number");
+	return 0;
+}
